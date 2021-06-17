31a09e767f520a8ac4280597f670c4fa58a06134
raxonpc <tomeczekorlowski1@gmail.com>
2021-06-18 00:43:25 +0200

    Follow enumerator conventions and use the term 'gender'

---
+++ Gender.hpp
@@ -0,0 +1,15 @@
+#ifndef GENDER_HPP
+#define GENDER_HPP 
+
+enum class Gender {
+	agender,
+	androgynous,
+	female,
+	genderfluid,
+	intersex,
+	male,
+	nonbinary,
+	other,
+};
+
+#endif /* GENDER_HPP */
