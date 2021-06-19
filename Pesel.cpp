f5979e68943355ebaa75d1557e973b464657ddc5
Jakub Stępień <macista.bukaj@gmail.com>
2021-06-19 16:24:46 +0200

    add Pesel struct

---
+++ Pesel.cpp
@@ -0,0 +1,105 @@
+#include "Pesel.hpp"
+
+int Pesel::get_day()
+{
+    return (date_of_birth_%100);
+}
+
+int Pesel::get_month()
+{
+    return ((date_of_birth_/100)%20);
+}
+
+int Pesel::get_year()
+{
+    int century=date_of_birth_%1e4;
+    int year=date_of_birth/1e4;
+    century/=20;
+    if (0==year){
+        year+=100;
+    }
+    if (4==century){
+        century=19;
+    }else{
+        century+=20;
+    }
+    year+=100*century;
+    return year;
+}
+
+int Gender Pesel::get_sex()
+{
+    if (serial_number_&1){
+        return Gender::male;
+    }
+    return Gender::female;
+}
+
+bool Pesel::validation_data()
+{
+    int day=get_day();
+    int month=get_month();
+    int year=get_year();
+    if (1>month||month>12 || day<1){
+        return true;
+    }
+    if (2==month){
+        if(!year&3){
+            if(year%25==0){
+                if(!year&15){
+                    if(day>29){
+                        return true;
+                    }
+                }else{
+                    if(day>28){
+                        return true;
+                    }
+                }
+            }else{
+                if(day>29){
+                    return true;
+                }
+            }
+        }else{
+            if(day>28){
+                return true;
+            }
+        }
+    }else if(((month&1) && (month<7)) || ((!(month&1)) && (month>6)){
+        if(day>31){
+            return true;
+        }
+    }else{
+        if(day>30){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Pesel::validation_check_digit()
+{
+    uint8_t check_digit=0;
+    uint32_t date_of_birth=date_of_birth_;
+    uint16_t serial_number=serial_number_;
+    check_digit+=(date_of_birth%10)*3;
+    date_of_birth/=10;
+    check_digit+=(date_of_birth%10);
+    date_of_birth/=10;
+    check_digit+=(date_of_birth%10)*9;
+    date_of_birth/=10;
+    check_digit+=(date_of_birth%10)*7;
+    date_of_birth/=10;
+    check_digit+=(date_of_birth%10)*3;
+    date_of_birth/=10;
+    check_digit+=(date_of_birth%10);
+    check_digit+=(serial_number%10)*3;
+    date_of_birth/=10;
+    check_digit+=(serial_number%10);
+    date_of_birth/=10;
+    check_digit+=(serial_number%10)*9;
+    date_of_birth/=10;
+    check_digit+=(serial_number%10)*7;
+    check_digit%=10;
+    return check_digit!=check_digit_;
+}
---
+++ Pesel.hpp
@@ -0,0 +1,52 @@
+#ifndef PESEL_HPP
+#define PESEL_HPP
+
+#include "Gender.hpp"
+
+#include <cstdbool>
+#include <cstdint>
+
+/**
+ *A structure that stores a PESEL number (Universal Electronic System for Registration of the Population).
+ * @autors Jakub Stępień
+ */
+struct Pesel
+{
+    /**
+     * Date of birth in the owner of the PESEL number in the YYMMDD format.
+     */
+    uint32_t date_of_birth_;
+    /**
+     * Serial number with gender indication.
+     */
+    uint16_t serial_number_;
+    /**
+     * Check digit.
+     */
+    uint8_t check_digit_;
+
+    /**
+     * The day of the birth month of the owner of the PESEL number,
+     * if the PESEL number is correct, is from 1 to 31.
+     */
+    public int get_day();
+    /**
+     * Returns the month of year from 1 to 12, if the PESEL number is correct.
+     */
+    public int get_month();
+    /**
+     * The function returns year of birth of the owner of the PESEL number.
+     */
+    public int get_year();
+    /**
+     * Checks if the date in the PESEL number is correct.
+     */
+    public Gender get_sex();
+    private bool validation_data();
+    private bool validation_check_digit();
+    /**
+     * If the pesel is correct, it returns false.
+     */
+    public bool validation();
+};
+#endif /* PESEL_HPP */
