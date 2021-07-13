b6b3f826a09bc65c401056523a15c77768b66dc1
raxonpc <tomeczekorlowski1@gmail.com>
2021-07-13 13:14:58 +0200

    Use vector and shared-pointer

--- StudentBase.hpp
+++ StudentBase.hpp
@@ -1,32 +1,34 @@
 #ifndef STUDENTBASE_HPP
 #define STUDENTBASE_HPP
-#include "Student.hpp"
-#include <forward_list>
+#include <memory>
 #include <optional>
+#include <vector>
+
+#include "Student.hpp"
 
 class StudentBase {
-public:
-  StudentBase() = default;
+   public:
+    StudentBase() = default;
 
-  // Add student
-  void add_student(const Student &);
-  // Display students list
-  void display_list() const;
+    // Add student
+    void add_student(Student&);
+    // Display students list
+    void display_list() const;
 
-  // Sorting functions
-  void sort_by_pesel();
-  void sort_by_surname();
+    // Sorting functions
+    void sort_by_pesel();
+    void sort_by_surname();
 
-  void erase_by_id(unsigned int);
+    void erase_by_id(unsigned int);
 
-  void write_to_file(const std::string &) const;
-  void read_from_file(const std::string &);
+    void write_to_file(const std::string&) const;
+    void read_from_file(const std::string&);
 
-  [[nodiscard]] std::optional<Student> search_by_pesel(const Pesel &) const;
-  [[nodiscard]] std::optional<Student>
-  search_by_surname(const std::string &) const;
+    [[nodiscard]] std::optional<Student> search_by_pesel(const Pesel&) const;
+    [[nodiscard]] std::optional<Student> search_by_surname(
+        const std::string&) const;
 
-private:
-  std::forward_list<Student> student_list_;
+   private:
+    std::vector<std::shared_ptr<Student> > student_list_;
 };
 #endif /* STUDENTBASE_HPP */
