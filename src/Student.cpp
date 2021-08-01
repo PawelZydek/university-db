e6cc3b8a35fd5d186e0c20e467d44fee0d589bdb
raxonpc <tomeczekorlowski1@gmail.com>
2021-08-01 14:37:27 +0200

    Set up Student class

---
+++ src/Student.cpp
@@ -0,0 +1,19 @@
+#include "Student.hpp"
+
+Student::Student(const std::string& name,
+                 const std::string& surname,
+                 const std::string& address,
+                 const Pesel& pesel,
+                 Gender gender,
+                 unsigned int index_num)
+    : Person{name, surname, address, pesel, gender}, index_num_{index_num} {}
+
+Student::Student(std::string&& name,
+                 std::string&& surname,
+                 std::string&& address,
+                 Pesel&& pesel,
+                 Gender gender,
+                 unsigned int index_num)
+    : Person{std::move(name), std::move(surname), std::move(address),
+             std::move(pesel), gender},
+      index_num_{index_num} {}
---
+++ src/Student.hpp
@@ -0,0 +1,38 @@
+#ifndef STUDENT_HPP
+#define STUDENT_HPP
+
+#include "Person.hpp"
+
+class Student : public Person {
+   public:
+    Student(const std::string&,
+            const std::string&,
+            const std::string&,
+            const Pesel&,
+            Gender,
+            unsigned int);
+
+    Student(std::string&&,
+            std::string&&,
+            std::string&&,
+            Pesel&&,
+            Gender,
+            unsigned int);
+
+    Student(const Student& student) = default;
+    Student(Student&& student) = default;
+
+    Student& operator=(const Student& student) = default;
+    Student& operator=(Student&& student) = default;
+
+    // Getter
+    [[nodiscard]] unsigned int get_index_num() const { return index_num_; }
+
+    // Setter
+    void set_index_num(unsigned int index_num) { index_num_ = index_num; }
+
+   private:
+    unsigned int index_num_{};
+};
+
+#endif /* STUDENT_HPP */
