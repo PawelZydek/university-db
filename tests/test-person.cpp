08aa9cbedde42bd5065613ac8dd5e51a9eab232a
raxonpc <tomeczekorlowski1@gmail.com>
2021-08-01 19:00:06 +0200

    Add input test for Person

--- tests/test-person.cpp
+++ tests/test-person.cpp
@@ -24,3 +24,20 @@ SCENARIO("Should output the Person to a stream", "[person][stream][output]") {
         }
     }
 }
+
+SCENARIO("Should parse the Person from a stream", "[person][stream][input]") {
+    GIVEN("A Person object") {
+        Person person{"Jan", "Kowalski", "Warszawa",
+                       Pesel{{7, 0, 0, 8, 0, 8, 3, 7, 1, 7, 1}}, Gender::male};
+        std::stringstream stream{"Jan,Kowalski,Warszawa,70080837171,male"};
+        
+        WHEN("It's parsed from a stream") {
+            Person personInput{};
+            stream >> personInput;
+
+            THEN("It should be in CSV format") {
+                REQUIRE(personInput == person);
+            }
+        }
+    }
+}
