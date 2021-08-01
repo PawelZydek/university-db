4f2513aeeffb43c4938f3c9d7ed2a1142bdea470
raxonpc <tomeczekorlowski1@gmail.com>
2021-08-01 14:06:32 +0200

    Implement getters & setters & constructors

--- src/Person.cpp
+++ src/Person.cpp
@@ -3,13 +3,21 @@
 Person::Person(const std::string& name, const std::string& surname, const std::string& address, 
 const Pesel& pesel, Gender gender)
     : name_{name}, surname_{surname}, address_{address}, pesel_{pesel}, gender_{gender} 
-{
-}
+    {
+        if(!pesel_.is_valid())
+        {
+            throw std::runtime_error{"not a valid pesel"};
+        }
+    }
 
 Person::Person(std::string&& name, std::string&& surname, std::string&& address, Pesel&& pesel, Gender gender)
     : name_{std::move(name)}, surname_{std::move(surname)}, address_{std::move(address)}, 
     pesel_{std::move(pesel)}, gender_{gender}
     {
+        if(!pesel_.is_valid())
+        {
+            throw std::runtime_error{"not a valid pesel"};
+        }
     }
 
 Person::~Person()
--- src/Person.hpp
+++ src/Person.hpp
@@ -11,11 +11,31 @@ class Person
 public:
     Person(const std::string&, const std::string&, const std::string&, const Pesel&, Gender);
     Person(std::string&&, std::string&&, std::string&&, Pesel&&, Gender);
-    Person() = default;
+    Person(const Person& person) = default;
+    Person(Person&& person) = default;
+
+    Person& operator= (const Person& person) = default;
+    Person& operator= (Person&& person) = default;
 
     virtual ~Person();
 
-    
+    // Getters
+    [[nodiscard]] std::string get_name() const      { return name_; }
+    [[nodiscard]] std::string get_surname() const   { return surname_; }
+    [[nodiscard]] std::string get_address() const   { return address_; }
+    [[nodiscard]] Pesel get_pesel() const           { return pesel_; }
+    [[nodiscard]] Gender get_gender() const         { return gender_; }
+
+    // Setters
+    void set_name(std::string&& name)               { name_ = name; }
+    void set_name(const std::string& name)          { name_ = name; }
+    void set_surname(std::string&& surname)         { surname_ = surname; }
+    void set_surname(const std::string& surname)    { surname_ = surname; }
+    void set_address(std::string&& address)         { address_ = address; }
+    void set_address(const std::string& address)    { address_ = address; }
+    void set_pesel(Pesel&& pesel)                   { pesel_ = pesel; }
+    void set_pesel(const Pesel& pesel)              { pesel_ = pesel; }
+    void set_gender(Gender gender)                  { gender_ = gender; }
 
 private:
     std::string name_{};
